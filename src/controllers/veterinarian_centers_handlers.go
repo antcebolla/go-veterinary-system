7ec08e2c208e8c64af20b95e83ac00edde41e97c
Antonio Alvarado <antoniojalvarado53@gmail.com>
2025-08-15 19:35:10 -0400

    get a center by id added

--- src/controllers/veterinarian_centers_handlers.go
+++ src/controllers/veterinarian_centers_handlers.go
@@ -15,6 +15,33 @@ func GetAllVetCentersHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, VetCenter)
 }
 
+func GetVeterinarianCenterByIdHandler(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Invalid veterinary center id",
+		})
+		return
+	}
+
+	center := models.VeterinaryCenter{}
+	err := database.DB.First(&center, id).Error
+	if err == gorm.ErrRecordNotFound {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Veterinary center not found",
+		})
+		return
+	}
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to get veterinary center, invalid id",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, center)
+}
+
 func CreateVetCenterHandler(c *gin.Context) {
 	var VetCenter models.VeterinaryCenter
 	err := c.ShouldBindJSON(&VetCenter)
@@ -52,8 +79,8 @@ func DeleteCenterHandler(c *gin.Context) {
 		return
 	}
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to delete veterinary center",
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to delete veterinary center, invalid id",
 		})
 		return
 	}
